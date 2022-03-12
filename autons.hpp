9f5548c7b4a986b1c55b9ff0cbbad6a09d4a4188
sshivv <yoboi1010@gmail.com>
2022-03-11 19:49:52 -0800

    Update autons.hpp

--- autons.hpp
+++ autons.hpp
@@ -20,12 +20,12 @@ namespace aut
         spin_lift(5000);
         drive(-390, 124.5, 1400);
         rotate_to(-90, 1500, 1.1);
-        drive(500, 120, 1300);
+        drive(500, 120, 1400);
         rotate_to(-1.5, 1800, 1.05);
-        drive(3600, 124.5, 3000, 2000);
+        drive(3650, 124.5, 3200, 2000);
         front_clamp.toggle();
         toggle_2bar();
-        rotate_to(33, 1800, 1.2);
+        rotate_to(35, 1800, 1.2);
         spin_dist(-1700);
         toggle_2bar();
         goal_cover.toggle();
@@ -52,16 +52,16 @@ namespace aut
     void back_neu() // start backward; neutral goal -> middle goal -> alliance goal
     {
         spin_lift(2500);
-        spin_dist(-520, 124.5, 2000);
+        spin_dist(-580, 124.5, 2000);
         toggle_2bar();
-        spin_dist(-950, 124.5, 2500);
+        spin_dist(-860, 124.5, 2500);
         goal_cover.toggle();
         spin_lift(-2000);
         spin_dist(-120, 124.5, 500);
         toggle_2bar();
-        rotate_to(86, 1500);
-        spin_dist(500, 90);
-        spin_dist(740, 70);
+        rotate_to(87, 1500);
+        spin_dist(300, 90);
+        spin_dist(500, 70);
         mtr::stop(mtr::chas);
         spin_lift(-700);
         delay(400);
@@ -71,14 +71,30 @@ namespace aut
         delay(300);
         spin_lift(4300);
         delay(400);
-        rotate_to(123, 1500, 1.3);
+        rotate_to(130, 1500, 1.3);
         drive(-1700, 100, 2200);
-        rotate_to(-84, 1500, 1.2);
+        rotate_to(280, 1500);
         drive(800, 120, 1500);
     }
 
-    void back_tree() // start backward; middle goal -> neutral goal -> alliance goal
+    void rush_tree() // start backward; middle goal -> neutral goal -> alliance goal
     {
+        spin_dist(2200);
+        chain_clamp.toggle();
+        spin_lift(5000);
+        rotate_to(-50, 1500);
+        toggle_2bar();
+        spin_dist(-1000);
+        goal_cover.toggle();
+        spin_dist(-150, 125, 150);
+        toggle_2bar();
+        rotate_to(-125, 1500, 1.3);
+        drive(1200, 3000);
+        rotate_to(117, 2000, 1.2);
+        drive(900);
+
+
+        /*
         spin_lift(2500);
         spin_dist(-520, 120, 2000);
         toggle_2bar();
@@ -106,25 +122,25 @@ namespace aut
         drive(-1200, 100, 2200);
         rotate_to(-73.6, 1500, 1.2);
         drive(750, 120, 1500);
+        */
     }
 
     void elev_neu()
     {
-        spin_dist(1430);
+        spin_dist(1440);
         chain_clamp.toggle();
         spin_dist(50, 50);
         spin_lift(5000);
-        drive(-1080);
-        rotate_to(-40, 1600);
-        spin_dist(1200);
-        spin_dist(200, 70);
-        front_clamp.toggle();
-        spin_dist(100);
-        delay(50);
-        drive(-1500);
+        rotate_to(90, 1100);
         toggle_2bar();
-        rotate_to(-85, 1700);
-        drive(-500);
+        spin_dist(-800);
+        goal_cover.toggle();
+        spin_dist(-100, 100, 300);
+        toggle_2bar();
+        rotate_to(135, 1500);
+        drive(1700, 3000);
+        rotate_to(87, 1500, 1.2);
+        drive(800, 125, 2000);
     }
 
     void rush_elev()
@@ -160,7 +176,7 @@ namespace aut
 
     }
 
-    void rush_tree() // elev side
+    void back_tree() // elev side
     {
         spin_dist(2000, 127, 2000);
         spin_dist(100, 80, 200);
@@ -172,8 +188,8 @@ namespace aut
 
 
     // callable list of autons
-    std::vector<void (*)()>  auton_calls{ solo_awp,   awp_elev,   back_neu,   back_tree,   elev_neu,   rush_elev,   rush_de_elev,   rush_tree };
-    std::vector<std::string> auton_names{"solo_awp", "awp_elev", "back_neu", "back_tree", "elev_neu", "rush_elev", "rush_de_elev", "rush_tree"};
+    std::vector<void (*)()>  auton_calls{ solo_awp,   awp_elev,   elev_neu,   rush_tree,   back_neu,   rush_elev,   rush_de_elev,   back_tree };
+    std::vector<std::string> auton_names{"solo_awp", "awp_elev", "elev_neu", "rush_tree", "back_neu", "rush_elev", "rush_de_elev", "back_tree"};
 }
 
 
